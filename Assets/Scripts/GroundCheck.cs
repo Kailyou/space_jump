4bafe7207091f2ac289e10182b60b3b56596c634
Kailyou <maik.thielen@gmail.com>
2016-02-28 11:01:35 +0100

    Player changes
    changes the way of moving and other stuffs.

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -6,18 +6,18 @@ public class GroundCheck : MonoBehaviour {
 	private Player player;
 
 	void Start() {
-		player = gameObject.GetComponentInParent<Player> ();
+		//player = gameObject.GetComponentInParent<Player> ();
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		player.grounded = true;
+		//player.grounded = true;
 	}
 
 	void OnTriggerStay2D(Collider2D collider) {
-		player.grounded = true;
+		//player.grounded = true;
 	}
 
 	void OnTriggerExit2D(Collider2D collider) {
-		player.grounded = false;
+		//player.grounded = false;
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,100 +4,36 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	
 	// configuration
-	public float maxVelocity = 10;
-	public float speed       = 30;
-	public float jumpPower   = 250f;
-	public int   maxHealth   = 100;
+	public float maxSpeed	 = 10;
+	public float jumpForce   = 550;
+	[HideInInspector]
+	public bool lookingRight = true;
+	public bool idling       = false;
+	public bool crouching    = false;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+	private bool grounded    = false;
 
 	// refs
 	private Rigidbody2D rb2d;
 	private Animator    animator;
-
-	// status vars
-	private bool isMirrored    = false;
-	private bool canDoubleJump = false;
-	private bool isCrouching   = false;
-	private bool died          = false;
-	private bool idling        = false;
-
-	// externally set
-	public bool grounded = false; // Ridgidbody2D
 
 	//timer
 	private float timer = 3f;
 
 	// Use this for initialization
 	void Start () {
-		rb2d = gameObject.GetComponent<Rigidbody2D>();
-		animator = gameObject.GetComponent<Animator>();
-	}
+		rb2d = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 
-	private void do_jump() {
-		if(grounded) {
-			rb2d.AddForce(Vector2.up*jumpPower);
-			canDoubleJump = true;
-		}
-		else if(canDoubleJump) {
-			rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-			rb2d.AddForce(Vector2.up*jumpPower);
-			canDoubleJump = false;
-		}
+		animator.SetBool ("crouching", false);
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
-
-		if (died) {
-			return;
-		}
-
-		/* animations */
-
-		animator.SetBool ("grounded", grounded);
-		animator.SetFloat ("speed", Mathf.Abs(rb2d.velocity.x));
-		animator.SetBool ("crouching", isCrouching);
-
-		if (died) {
-			animator.SetBool("is_dead", true);
-			//gameoverMenu.onGameOver(!isLocalPlayer);
-			return;
-		}
-
-
-		/* movement */
-
-		// walk left image transformation
-		if (!isMirrored && rb2d.velocity.x < -0.1) {
-			Vector3 tmp = transform.localScale;
-			transform.localScale = new Vector3(tmp.x*-1, tmp.y, tmp.z);
-			isMirrored = true;
-		}
-		// walk right image transformation
-		if (isMirrored && rb2d.velocity.x > 0.1) {
-			Vector3 tmp = transform.localScale;
-			transform.localScale = new Vector3(tmp.x*-1, tmp.y, tmp.z);
-			isMirrored = false;
-		}
-
-		// jumping
-		if (!isCrouching) {
-			if (Input.GetButtonDown("Jump"))
-				do_jump ();
-			if (Input.GetMouseButtonDown(0))
-				do_jump ();
-
-			foreach (var touch in Input.touches) {
-				if (touch.phase == TouchPhase.Began) {
-					do_jump ();
-					break;
-				}
-			}
-		}
-	
-		/* others */
-
+		
 		//idle timer
-		if (rb2d.velocity == Vector2.zero) {
+		if (rb2d.velocity.x <= 0.1f) {
 			idling = true;
 		} else {
 			idling = false;
@@ -106,45 +42,32 @@ public class Player : MonoBehaviour {
 
 		if(idling) {
 			timer -= Time.deltaTime;
-
-			animator.SetBool ("idling", timer <= 0f);
 		}
+			
+		animator.SetBool ("idling", timer <= 0f);
 	}
 
-	void FixedUpdate() {
-
+	void FixedUpdate()	{
+		
 		// walking left or right
-		if (!isCrouching) {
-			
-			float moveHorizontal  = Input.GetAxis ("Horizontal");
-			float moveVertical    = Input.GetAxis ("Vertical");
+		float hor = Input.GetAxis("Horizontal");
 
-			// stop walking immediatly when the user releases the button
-			//Vector3 tmp = rb2d.velocity;
-			//tmp.x *= 0.75f;
-			//rb2d.velocity = tmp;
+		animator.SetFloat ("speed", Mathf.Abs (hor));
 
+		rb2d.velocity = new Vector2 (hor * maxSpeed, rb2d.velocity.y);
 
+		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.15F, whatIsGround);
 
-			/*test
-			 * 		//hier wird physik berechnet
-			float hor = Input.GetAxis ("Horizontal");
-			anim.SetFloat ("Speed", Mathf.Abs (hor));
+		animator.SetBool ("grounded", grounded);
 
-			rb2d.velocity = new Vector2 (hor * MAX_SPEED, rb2d.velocity.y);
-			*/
+		if ((hor > 0 && !lookingRight) || (hor < 0 && lookingRight)) 
+			Flip ();
+	}
 
-			//rb2d.velocity = new Vector2 (h * speed, rb2d.velocity.y);
-
-			//rb2d.AddForce (Vector2.right * speed * h);
-			//rb2d.AddForce (Vector2.right * speed * Input.acceleration.x);
-
-
-			if (rb2d.velocity.x > maxVelocity) {
-				rb2d.velocity = new Vector2 (maxVelocity, rb2d.velocity.y);
-			} else if (rb2d.velocity.x < -maxVelocity) {
-				rb2d.velocity = new Vector2 (-maxVelocity, rb2d.velocity.y);
-			}
-		}
+	public void Flip() {
+		lookingRight = !lookingRight;
+		Vector3 myScale = transform.localScale;
+		myScale.x *= -1;
+		transform.localScale = myScale;
 	}
 }

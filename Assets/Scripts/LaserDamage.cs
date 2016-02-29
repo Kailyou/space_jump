9bb1048a0be5da2be6dbeca167620b4d3afb0d20
Kailyou <maik.thielen@gmail.com>
2016-02-29 07:14:23 +0100

    changes to laser
    laser now will do damage and be destroyed after collision.

---
+++ Assets/Scripts/LaserDamage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserDamage : MonoBehaviour {
+
+	public float damage = 1;
+
+	//if the laser collides with another 2d collider which is not the player's one
+	//execute the ApplyDamage function, there will be no error if this function is not implemented.
+	//laser will be destroyed itself after collision.
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("test");
+		if (!other.CompareTag ("Player")) {
+			other.SendMessage ("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+			Destroy (gameObject);
+		}
+	}
+}

2c3ad172fa476b00cd77d301163c481a2757e6dd
M1cha <sigmaepsilon92@gmail.com>
2016-03-30 16:42:51 +0200

    simplify laser reach detection

---
+++ Assets/objects/laser/scripts/LaserDamage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserDamage : MonoBehaviour {
+
+	public float damage = 1;
+
+	private Vector3 initialPosition;
+
+	void Start() {
+		initialPosition = transform.position;
+	}
+
+	void FixedUpdate ()
+	{
+		if (Vector3.Distance (initialPosition, transform.position) >= 5) {
+			Destroy (gameObject);
+		}
+	}
+
+	// if the laser collides with another 2d collider which is not the player's one
+	// execute the ApplyDamage function, there will be no error if this function is not implemented.
+	// laser will be destroyed itself after collision.
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!other.CompareTag ("Player")) {
+			other.SendMessage ("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+			Destroy (gameObject);
+		}
+	}
+}

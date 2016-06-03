0679a183556bfa57f18523c030576f0f29aa0d8c
Kailyou <maik.thielen@gmail.com>
2016-06-03 12:11:43 +0200

    level 1: background and ground

---
+++ Assets/backgroundScroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Script which will set a repeatable background for the ice level
+
+public class backgroundScroll : MonoBehaviour 
+{
+	void Update ()
+	{
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x += Time.deltaTime / 10f;
+
+		mat.mainTextureOffset = offset;
+	}
+}

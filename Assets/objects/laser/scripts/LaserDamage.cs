81e78de55365a3abe1d610a0268399847c6815d3
Kailyou <maik.thielen@gmail.com>
2016-09-20 16:03:20 +0200

    Bugfix: LaserDamage.cs
    Laser now will not be dissapear when hitting an invisible bound.

--- Assets/objects/laser/scripts/LaserDamage.cs
+++ Assets/objects/laser/scripts/LaserDamage.cs
@@ -21,9 +21,12 @@ public class LaserDamage : MonoBehaviour {
 	// if the laser collides with another 2d collider which is not the player's one
 	// execute the ApplyDamage function, there will be no error if this function is not implemented.
 	// laser will be destroyed itself after collision.
-	void OnTriggerEnter2D(Collider2D other) {
-		if (!other.CompareTag ("Player") && !other.CompareTag ("ground_bounds")) {
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!other.CompareTag ("Player") && !other.CompareTag ("ground_bounds") && !other.CompareTag("Bound"))
+		{
 			other.SendMessage ("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+
 			Destroy (gameObject);
 		}
 	}

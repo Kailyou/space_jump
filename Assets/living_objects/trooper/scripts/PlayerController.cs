878178ea813d89673c9d653fe20513b3b79a5b57
Kailyou <maik.thielen@gmail.com>
2016-09-27 20:09:56 +0200

    Changes: PlayerController
    - Changed structore, fixedUpdate now will be used only for physics
    interaction
    - Added jump
    - minor fixes

--- Assets/living_objects/trooper/scripts/PlayerController.cs
+++ Assets/living_objects/trooper/scripts/PlayerController.cs
@@ -21,18 +21,28 @@ public class PlayerController : MonoBehaviour
 	[HideInInspector]
 	public bool idling = false;
 	[HideInInspector]
-	private bool crouching = false;
 	private bool grounded  = false;
-	private bool jumping   = false;
 	private float timer = 3f;
 	private bool do_lock = false;
 
-	// Attack
-	private float attackCooldownTime        = 1.5f;
-	private float currentAttackCooldownTime = 0f;
-	private bool attackOnCD = false;
-	private bool attacking = false;
+	// Jump
+	private bool prepareJump = false;
+	private bool jumping     = false;
+	private bool jumpLock    = false;
 
+
+	/* Attack */
+	// Melee Attack
+	private float attackCooldownTime_melee        	= 0.5f;
+	private float currentAttackCooldownTime_melee	= 0f;
+	private bool attackOnCD_melee 				  	= false;
+	private bool attacking_melee 					= false;
+
+	// Range Attack
+	private float attackCooldownTime_range        	= 5f;
+	private float currentAttackCooldownTime_range	= 0f;
+	private bool attackOnCD_range 					= false;
+	private bool attacking_range 					= false;
 
 	// Collision detection
 	public LayerMask whatIsGround;
@@ -42,37 +52,22 @@ public class PlayerController : MonoBehaviour
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
-
-		animator.SetBool ("crouching", false);
 	}
 
 	void Update ()
 	{		
-		// Update attack cooldown time
-		if (attackOnCD)
+		handleUserInput();
+
+		updateAttack ();
+
+		updateJump ();
+					
+		// Change direction of the player by inverting the scale position
+		float hor = Input.GetAxis ("Horizontal");
+
+		if ((hor > 0 && !lookingRight) || (hor < 0 && lookingRight))
 		{
-			currentAttackCooldownTime += Time.deltaTime;
-
-			if (currentAttackCooldownTime > attackCooldownTime)
-			{
-				attackOnCD = false;
-				currentAttackCooldownTime = 0f;
-			}
-		}
-
-		if (!do_lock) 
-		{
-			// Jump
-			if (Input.GetButtonDown ("Jump") && grounded) 
-			{
-				jumping = true;
-			}
-
-			// Laser attack
-			if (Input.GetButtonDown ("Fire1") && !attacking && !attackOnCD)
-			{
-				attacking = true;
-			}
+			Flip ();
 		}
 
 		// Idle 
@@ -85,43 +80,57 @@ public class PlayerController : MonoBehaviour
 			timer = 3f;
 		}
 
-		if (idling) 
-		{
-			timer -= Time.deltaTime;
-		}
-			
-		animator.SetBool ("idling", timer <= 0f);
+		updateTimer ();
 	}
 
-	void FixedUpdate ()
+	private void handleUserInput()
 	{
-		if (!do_lock) {
-			// Walking left or right
-			float hor = Input.GetAxis ("Horizontal");
-			rb2d.velocity = new Vector2 (hor * maxSpeed, rb2d.velocity.y);
+		/* CONTROLLING */
+		if (!do_lock) 
+		{
+			// Jump
+			if (Input.GetButtonDown ("Jump") && grounded && !prepareJump && !jumping) 
+			{
+				prepareJump = true;
+			}
 
-			// Change direction of the player by inverting the scale position
-			if ((hor > 0 && !lookingRight) || (hor < 0 && lookingRight))
-				Flip ();
+			// Melee Attack
+			if (Input.GetButtonDown ("Fire1") && !attacking_melee && !attackOnCD_melee && !attacking_range)
+			{
+				attacking_melee = true;
+			}
+
+			// Range Attack
+			if (Input.GetButtonDown ("Fire2") && !attacking_range && !attackOnCD_range && !attacking_melee)
+			{
+				attacking_range = true;
+			}
+		}
+	}
+
+
+	private void updateAttack()
+	{
+		// Melee
+		if (attacking_melee) 
+		{
+			attackOnCD_melee = true;
+
+			Debug.Log ("test");
+
+			animator.SetTrigger ("attacking_melee");
+
+			attacking_melee = false;
 		}
 
-		// Checks if the player is grounded by checking if the ground check position of the player
-		// colides with any other circle
-		// the both hit box circles of the player are excluded.
-		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.15F, whatIsGround);
-
-		// Jump
-		if (jumping) 
+		// Range
+		if (attacking_range)
 		{
-			rb2d.AddForce (new Vector2 (0, jumpForce));
-			jumping = false;
-		}
+			attackOnCD_range = true;
 
-		// Attack
-		if (attacking)
-		{
-			attackOnCD = true;
-			animator.SetTrigger ("attacking");
+			animator.SetTrigger ("attacking_range");
+
+			// Create laser object and adds a force to the looking side of the player
 			GameObject laser = (GameObject)Instantiate (laserPrefab, laserSpawnPoint.position, Quaternion.identity);
 			laser.tag = "Laser";
 
@@ -133,12 +142,107 @@ public class PlayerController : MonoBehaviour
 				laser.GetComponent<Rigidbody2D> ().AddForce (Vector3.left * laserSpeed);
 			}
 
-			attacking = false;
+			attacking_range = false;
+		}
+	}
+
+
+	// Handles the jump of the trooper
+	// When the tropper is jumping and reaches the ground
+	// the jump will stop.
+	private void updateJump()
+	{
+		// Check if jump is done
+		if (prepareJump) 
+		{
+			if (!grounded)
+			{
+				animator.SetBool ("jumping", true);
+				jumping     = true;
+				prepareJump = false;
+				jumpLock    = true;
+			}
+		} 
+
+		else if (jumping) 
+		{
+			if (grounded) 
+			{
+				animator.SetBool ("jumping", false);
+				jumping = false;
+				jumpLock = false;
+			}
+		}
+	}
+
+
+	private void updateTimer()
+	{
+		// Melee Attack Cooldown
+		if (attackOnCD_melee)
+		{
+			currentAttackCooldownTime_melee += Time.deltaTime;
+
+			if (currentAttackCooldownTime_melee > attackCooldownTime_melee)
+			{
+				attackOnCD_melee = false;
+				currentAttackCooldownTime_melee = 0f;
+			}
 		}
 
+		// Range Attack Cooldown
+		if (attackOnCD_range)
+		{
+			currentAttackCooldownTime_range += Time.deltaTime;
+
+			if (currentAttackCooldownTime_range > attackCooldownTime_range)
+			{
+				attackOnCD_range = false;
+				currentAttackCooldownTime_range = 0f;
+			}
+		}
+
+		// Idle timer
+		if (idling) 
+		{
+			timer -= Time.deltaTime;
+		}
+
+		animator.SetBool ("idling", timer <= 0f);
+	}
+
+
+
+
+	void FixedUpdate ()
+	{
+		if (!do_lock)
+		{
+			// Walking left or right
+			float hor = Input.GetAxis ("Horizontal");
+
+			rb2d.velocity = new Vector2 (hor * maxSpeed, rb2d.velocity.y);
+		}
+
+		// Checks if the player is grounded by checking if the ground check position of the player
+		// colides with any other circle
+		// the both hit box circles of the player are excluded.
+		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.15F, whatIsGround);
 		animator.SetBool ("grounded", grounded);
+
+		// Jump
+		if (prepareJump && !jumpLock) 
+		{
+			rb2d.AddForce (new Vector2 (0, jumpForce));
+			jumping  = false;
+			jumpLock = true; 
+		}
+
 		animator.SetFloat ("speed", Mathf.Abs (rb2d.velocity.x));
 	}
+
+
+
 
 	public void Flip ()
 	{

8d3d16fe493d090d89a671ff04db2fd9b3f7a055
M1cha <sigmaepsilon92@gmail.com>
2016-10-01 14:22:08 +0200

    BoundsTrigger: add option 'position'

--- Assets/scripts/BoundsTrigger.cs
+++ Assets/scripts/BoundsTrigger.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class BoundsTrigger : MonoBehaviour {
 
+	public enum Position {Top, Right, Bottom, Left};
+
+	public Position position;
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.tag == "Wolf" || other.tag == "Seeker" || other.tag == "Golem")

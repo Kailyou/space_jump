3d7afdbc94c5bc4e8563a0af61ab00d64149bce2
Kailyou <maik.thielen@gmail.com>
2016-10-01 13:34:48 +0200

    Added: Ice Crystals which will fall
    - Added Ice Crystal
    - minor project structure changes

---
+++ Assets/level 1 - ice/Events/startIceCaveEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Small event, which will turn off the snow effect
+ * and lowers the light level
+ */
+
+/* REFERENCES */
+
+
+public class startIceCaveEvent : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

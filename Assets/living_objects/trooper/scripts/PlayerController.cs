7298cee6bb8315d327626227b4d4a44d55211738
M1cha <sigmaepsilon92@gmail.com>
2016-10-01 14:22:47 +0200

    PlayerController: exclude z-index >=1000 for ground check

--- Assets/living_objects/trooper/scripts/PlayerController.cs
+++ Assets/living_objects/trooper/scripts/PlayerController.cs
@@ -141,7 +141,7 @@ public class PlayerController : MonoBehaviour
 		// Checks if the player is grounded by checking if the ground check position of the player
 		// colides with any other circle
 		// the both hit box circles of the player are excluded.
-		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.5F, whatIsGround);
+		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.5F, whatIsGround, -Mathf.Infinity, 999);
 		animator.SetBool ("grounded", grounded);
 
 		animator.SetFloat ("speed", Mathf.Abs (rb2d.velocity.x));

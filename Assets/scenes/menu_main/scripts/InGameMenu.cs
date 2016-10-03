9650c070accb17e7aa116218a5fb3ef4fab8aeee
M1cha <sigmaepsilon92@gmail.com>
2016-10-03 18:47:01 +0200

    add in-game menu

---
+++ Assets/scenes/menu_main/scripts/InGameMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+
+	public GameObject menuObject;
+
+	private float nextEscTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		menuObject.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape) && Time.time > nextEscTime) {
+			nextEscTime = Time.time + 0.2f;
+			menuObject.active = !menuObject.active;
+		}
+	}
+
+	public void onButtonRestart() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void onButtonMenu() {
+		SceneManager.LoadScene ("Scenes/menu_main/scene");
+	}
+
+	public void onButtonExit() {
+		Application.Quit ();
+	}
+}

6fbde3d49fd66d49443b7d20987bf615dcb2fc80
Kailyou <maik.thielen@gmail.com>
2016-10-03 11:12:24 +0200

    Level 1 - Added level end event

---
+++ Assets/level 1 - ice/Events/EndLevel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndLevel : MonoBehaviour
+{
+	public PlayerController playerController;
+	public Text messageText;
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.CompareTag ("Player"))
+		{
+			if (playerController.collectAmount == playerController.maxCollectAmount) 
+			{
+				SceneManager.LoadScene ("Scenes/rocketcrash/rocketcrash");
+			} 
+			else 
+			{
+				messageText.text = "Collect all energy before continue travelling.";
+			}
+		}
+	}
+
+	private void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.CompareTag ("Player"))
+		{
+			messageText.text = "";
+		}
+	}
+}

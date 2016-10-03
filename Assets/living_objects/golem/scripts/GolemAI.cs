cc442a49f5139ff773df52ebfb41e4e74d28a30f
Kailyou <maik.thielen@gmail.com>
2016-10-03 08:36:10 +0200

    Small improvement golem

--- Assets/living_objects/golem/scripts/GolemAI.cs
+++ Assets/living_objects/golem/scripts/GolemAI.cs
@@ -56,6 +56,10 @@ public class GolemAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (enemyHealth.isDeath ())
+			return;
+
 		RaycastingHeardPlayer ();
 		RaycastingPlayerDetected ();
 		RaycastingPlayerInAttackRange ();

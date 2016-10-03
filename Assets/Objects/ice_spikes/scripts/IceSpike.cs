91f0f0a096a042b369d9f0b802333644a5f411a3
Kailyou <maik.thielen@gmail.com>
2016-10-03 07:41:23 +0200

    Bugfix: Ice Spike now will do continues damage over time more properly

---
+++ Assets/Objects/ice_spikes/scripts/IceSpike.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceSpike : MonoBehaviour
+{
+	/* CONFIG */
+	private float dotCooldownTime_melee	= 0.5f;
+	private float next_dot_time 		= 0f;
+	private int damage					= 1;
+
+	void OnCollisionStay2D (Collision2D other)
+	{
+		Debug.Log ("test");
+
+		if (other.collider.CompareTag ("Player")  && Time.time > next_dot_time)
+		{
+			next_dot_time = Time.time + dotCooldownTime_melee;
+
+			other.collider.SendMessage ("ApplyDamage", 1);
+		}
+	}
+}
